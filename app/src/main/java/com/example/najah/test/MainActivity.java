8b636df12c50ae62b2d4503a0a2bc5be4fbbd4b0
najehdhahri <nejah.dhahri@esprit.tn>
2017-11-27 23:55:03 +0100

    Initial commit

---
+++ app/src/main/java/com/example/najah/test/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.najah.test;
+
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    private CoordinatorLayout rootView;
+     private ImageButton addIsn;
+    private EditText nom;
+    private EditText adresse;
+    private EditText email;
+    private EditText tel;
+private  String urlpost;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rootView=(CoordinatorLayout) findViewById(R.id.rootview);
+        //View rooView=View.inflate(R.layout.activity_main,contai)
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        addIsn=(ImageButton) findViewById(R.id.buttonCnx);
+        nom=(EditText) findViewById(R.id.nom) ;
+        adresse=(EditText) findViewById(R.id.adresse) ;
+        email=(EditText) findViewById(R.id.email) ;
+        tel=(EditText) findViewById(R.id.tel) ;
+
+    //    ajouterins();
+      //  return rootView;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
